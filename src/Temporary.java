fb83a1a601bbbc1a8604b3a35b4073b01ee33e88
sumanth <saisumanth5318@gmail.com>
2022-11-24 17:07:13 -0500

    to string is modified

--- src/Temporary.java
+++ src/Temporary.java
@@ -1,6 +1,34 @@
 public class Temporary extends Contract{
+    private double hourlySalary = 0;
+    private int accumulatedHours= 0;
+    private double accumulatedsalary;
+    public Temporary(double hourlySalary,int accumulatedHours){
+        this.hourlySalary = hourlySalary;
+        this.accumulatedHours =accumulatedHours;
+    }
 
-    public Temporary(int nbChildren,boolean married,int bonusPerChildPerMonth,int accumulatedDays,double monthlySalary,double hourlySalary,int accumulatedHours,String type) {
-        super(nbChildren,married,bonusPerChildPerMonth,accumulatedDays,monthlySalary,hourlySalary,accumulatedHours,type);
+    public double getHourlySalary() {
+        return hourlySalary;
+    }
+
+    public int getAccumulatedHours() {
+        return accumulatedHours;
+    }
+
+    public double getAccumulatedsalary() {
+        return accumulatedsalary;
+    }
+
+    public void setAccumulatedsalary(double accumulatedsalary) {
+        this.accumulatedsalary = accumulatedsalary;
+    }
+
+    @Override
+    public String toString() {
+        return  "he is a temporary employee with "+getHourlySalary()+" hourly salary and he has worked for "+getAccumulatedHours()+" hours.";
+    }
+    public double accumulatedsalary(){
+        accumulatedsalary = getHourlySalary()*getAccumulatedHours()*12;
+        return accumulatedsalary;
     }
 }
